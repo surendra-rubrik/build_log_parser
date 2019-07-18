7fe45f271f2c0fe9a8ccdf8f8bacf3e1eb553581
skonhor <subodh.konhor@rubrik.com>
2019-07-18 09:20:33 +0530

    Added tags for type of failure

--- parse.py
+++ parse.py
@@ -9,6 +9,16 @@ import collections
 import requests
 
 build_matrix = []
+JENKINS_URL = "http://cdm-builds.corp.rubrik.com/job/"
+REQUIRED_DETAILS = "duration,result,timestamp"
+
+INFRA_RELATED_FAILURE_SIGNATURE = [
+    "Caused: java.io.IOException: Backing channel",
+    'hudson.plugins.git.GitException: Command "git checkout -f',
+    "ERROR: error loading package '': Encountered error while reading"
+]
+
+
 
 def utc_time_duration(start_time,end_time):
     format = '%Y-%m-%dT%H:%M:%S%z'
@@ -17,9 +27,28 @@ def utc_time_duration(start_time,end_time):
     return str(end_time_utc - start_time_utc)
 
 
+def reassign_bug(bug_id, failure_matrix):
+    pass
+
+
+def check_for_non_test_failures(passed_line):
+    infra_failure_tags = [i for i in INFRA_RELATED_FAILURE_SIGNATURE if i in
+                           passed_line]
+    if infra_failure_tags:
+        failure_type = {
+            'failure_type': 'INFRA',
+            'bug_owner': 'manuel.flamerich@rubrik.com',
+            'error': infra_failure_tags[0]
+        }
+        return failure_type
+
+    return None
+
+
 def process_test_log(tmp_log,is_passed=True):
     #print(len(tmp_log))
     testcase_matrix = {}
+    failure_type = {}
     count = 0
     for line in tmp_log:
         if is_passed:
@@ -30,13 +59,22 @@ def process_test_log(tmp_log,is_passed=True):
                 status = 'PASSED'
                 testcase_matrix[key] = {'time_taken' : time_taken,'status':status}
         else:
-            if 'PASSED ' in line or 'FAILED ' in line:
+            if 'PASSED ' in line or 'FAILED ' in line or 'TIMEOUT ' in line:
                 key = re.sub(r'[^\x00-\x7F]+', '', line.split()[0])
                 time_taken = re.sub(r'[^\x00-\x7F]+', '', line.split()[3])
                 time_taken = time_taken.split(';')[1] if len(time_taken.split(';')) > 1 else time_taken
                 status = line.split()[1]
-                testcase_matrix[key] = {'time_taken': time_taken, 'status': status}
-    return testcase_matrix
+                if status in ['FAILED', 'TIMEOUT']:
+                    failure_type.update({
+                        'failure_type': 'TEST',
+                        'bug_owner': 'owner'
+                    })
+                testcase_matrix[key] = {
+                    'time_taken': time_taken,
+                    'status': status,
+                }
+
+    return testcase_matrix, failure_type
 
 
 def parse_console_content(fileName,buildNumber):
@@ -64,6 +102,7 @@ def parse_console_content(fileName,buildNumber):
     archiving_start_time = False
     archiving_end_time = False
     overral_status = 'FAILED'
+    type_of_failure = None
 
     tmp_log = []
 
@@ -79,6 +118,8 @@ def parse_console_content(fileName,buildNumber):
                 contentComplete = True
                 continue
             if contentFound and contentComplete == False:
+                if not "</b> </span>" in line:
+                    continue
                 time_stamp = (line.split("</b> </span>", 1)[0]).split('<span class="timestamp"><b>',1)[1]
                 line_content = line.split("</span>", 1)[1]
 
@@ -131,12 +172,14 @@ def parse_console_content(fileName,buildNumber):
                     tmp_log = []
                 if go_test_start_time and 'Go tests: ' in line_content:
                     go_test_end_time = time_stamp
-                    build_details['go_test_cases'] = process_test_log(tmp_log,True)
+                    build_details['go_test_cases'] = process_test_log(tmp_log)
                     tmp_log = []
                 elif go_test_start_time and not go_test_end_time and 'FAILURE SUMMARY:' in line_content:
                     go_test_end_time = time_stamp
-                    build_details['go_test_cases'] = process_test_log(tmp_log,False)
+                    build_details['go_test_cases'] = process_test_log(
+                        tmp_log, False)
                     tmp_log =[]
+
                     #break
 
                 if "bazel test '--test_tag_filters=unit-test' '--test_output=errors' //'src/cpp'/..." in line_content:
@@ -148,13 +191,24 @@ def parse_console_content(fileName,buildNumber):
                     tmp_log = []
                 elif cpp_test_start_time and not cpp_test_end_time and 'FAILURE SUMMARY:' in line_content:
                     cpp_test_end_time = time_stamp
-                    build_details['cpp_test_cases'] = process_test_log(tmp_log,False)
+                    build_details['cpp_test_cases'], build_details[
+                        'failure_type'] = process_test_log(
+                        tmp_log, False)
+                    build_details['failed_test_key'] = 'cpp_test_cases'
                     #break
 
                 if 'Running Bazel tests for Java: ' in line_content:
                     java_test_start_time = time_stamp
-                if 'Java tests: ' in line_content:
+                elif java_test_start_time and 'Java tests: ' in line_content:
                     java_test_end_time = time_stamp
+                    build_details['java_test_cases'] = process_test_log(tmp_log)
+                    tmp_log = []
+                elif java_test_start_time and not java_test_end_time and 'FAILURE SUMMARY:' in line_content:
+                    java_test_end_time = time_stamp
+                    build_details['java_test_cases'], build_details[
+                        'failure_type'] = process_test_log(
+                        tmp_log, False)
+                    build_details['failed_test_key'] = 'java_test_cases'
 
 
                 if 'Building Web and running tests...' in line_content:
@@ -174,9 +228,16 @@ def parse_console_content(fileName,buildNumber):
                 if 'Filed issue' in line_content:
                     bug_id = line_content.split('Filed issue u\'')[1].split('\'')[0]
                     build_details['bug_id'] = bug_id
+                    if 'failed_test_key' in build_details:
+                        reassign_bug(bug_id, build_details[build_details[
+                            'failed_test_key']])
 
 
                 tmp_log.append(line_content)
+                if not build_details.get('failure_type', None):
+                    build_details['failure_type'] = check_for_non_test_failures(
+                        line_content)
+
 
                 #print(time_stamp + ' ' +  line_content)
                 #print(line)
@@ -221,21 +282,31 @@ def parse_console_content(fileName,buildNumber):
         #build_details['status'] = overral_status
         build_details['compile_details'] = compile_details
 
-        build_rest_url = 'http://cdm-builds.corp.rubrik.com/job/Compile_CDM/%s/api/json?pretty=true'%(buildNumber)
+        build_rest_url = JENKINS_URL + \
+            'Compile_CDM/%s/api/json?tree=%s'%(buildNumber, REQUIRED_DETAILS)
         response = requests.get(build_rest_url)
         response = response.json()
         build_details['duration'] = response['duration']
-        build_details['result'] = overral_status
-        build_details['timestamp'] = response['timestamp']
+        build_details['result'] = response['result']
+        build_details['start_epoch_timestamp'] = response['timestamp']
+        build_details['end_epoch_timestamp'] = response['timestamp'] + response[
+            'duration']
+        if 'FAILED' in build_details['result'] and not build_details[\
+                'failure_type']:
+            build_details['failure_type'].update({
+                "bug_owner": "owner",
+                "failure_type": "UKNOWN"
+            })
         return build_details
 
+
 def get_build_file(build_number):
-    build_url= 'http://cdm-builds.corp.rubrik.com/job/Compile_CDM/%d/consoleFull'% (build_number)
-    subprocess.call(["wget",'-O' ,'data/%d.html' %(build_number),build_url])
+    build_url= JENKINS_URL + "Compile_CDM/%d/consoleFull"% (build_number)
+    subprocess.call(["wget", '-O', 'data/%d.html' %(build_number),build_url])
 
 
 def main():
-    for buildNumber in range(7005, 7006):
+    for buildNumber in range(7013, 7014):
         #get_build_file(buildNumber)
         #print('==============================================\n')
         build_matrix.append(parse_console_content('data/%d.html'%(buildNumber),buildNumber))
